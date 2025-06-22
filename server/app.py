195344f26627f74e42dbc58b3bd2b8132adac099
Annette <annette.ngugi@student.moringaschool.com>
2025-06-22 21:51:56 +0300

    complete lab

--- server/app.py
+++ server/app.py
@@ -14,13 +14,42 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/messages')
-def messages():
-    return ''
+@app.route("/messages", methods=["GET"])
+def get_messages():
+    messages = Message.query.order_by(Message.created_at.asc()).all()
+    return jsonify([msg.to_dict() for msg in messages])
 
-@app.route('/messages/<int:id>')
-def messages_by_id(id):
-    return ''
+@app.route("/messages", methods=["POST"])
+def create_message():
+    data = request.get_json()
+    if not data or not data.get("body") or not data.get("username"):
+        return make_response({"error": "Missing body or username"}, 400)
+
+    new_msg = Message(body=data["body"], username=data["username"])
+    db.session.add(new_msg)
+    db.session.commit()
+
+    return jsonify(new_msg.to_dict()), 201
+
+
+@app.route("/messages/<int:id>", methods=["PATCH"])
+def update_message(id):
+    message = Message.query.get_or_404(id)
+    data = request.get_json()
+
+    if "body" in data:
+        message.body = data["body"]
+
+    db.session.commit()
+    return jsonify(message.to_dict()), 200
+
+
+@app.route("/messages/<int:id>", methods=["DELETE"])
+def delete_message(id):
+    message = Message.query.get_or_404(id)
+    db.session.delete(message)
+    db.session.commit()
+    return jsonify({"message": f"Message {id} deleted"}), 200
 
 if __name__ == '__main__':
     app.run(port=5555)
